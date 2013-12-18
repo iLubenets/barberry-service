10b6aa067f222e388850288c33d792a3221d0e44
il <il@prostobank.com>
2013-12-18 16:10:38 +0200

    Simplified user check in index.php

--- public/index.php
+++ public/index.php
@@ -9,12 +9,15 @@ include dirname(__DIR__) . '/vendor/autoload.php';
 
 try {
     $config = Yaml::parse(dirname(__DIR__) . '/etc/config.yml');
-    $isAccessDenied = false;
     if ($_SERVER['REQUEST_METHOD'] !== 'GET'
         && $config['prosto_auth']['certified_ip'] !== $_SERVER['REMOTE_ADDR']) {
         $prostoAuth = new ProstoAuth($config['prosto_auth'], $config['database']);
         $user = $prostoAuth->getAuthenticateUser('teammember');
-        $isAccessDenied = $user === null;
+        if(!$user){
+            http_response_code(403);
+            echo 'Access denied!';
+            die();
+        }
     }
 } catch (ProstoAuthException $e) {
     http_response_code(404);
@@ -26,11 +29,5 @@ try {
     die();
 }
 
-if(!$isAccessDenied){
-    $application = new Application(new Config(realpath(__DIR__ . '/../'), '/etc/config.php'));
-    $application->run()->send();
-} else {
-    http_response_code(403);
-    echo 'Access denied!';
-    die();
-}
+$application = new Application(new Config(realpath(__DIR__ . '/../'), '/etc/config.php'));
+$application->run()->send();
